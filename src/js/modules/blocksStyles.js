82a41b763e16448fd4e1a2909bb3d70004bd3e18
Artem <mart_22@ukr.net>
2022-09-06 23:25:51 +0300

    blocksStyles.js: Add title class on tag H2

--- src/js/modules/blocksStyles.js
+++ src/js/modules/blocksStyles.js
@@ -1,1 +1,9 @@
-export default function blockStyles() {}
+export default function blockStyles() {
+	if (document.querySelector('h2')) {
+		document.querySelectorAll('h2').forEach((el) => {
+			if (el.nextElementSibling?.tagName === 'SPAN') {
+				el.classList.add('title-1');
+			}
+		});
+	}
+}
