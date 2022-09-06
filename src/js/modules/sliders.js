2f1c39a51d34602a8b1127ebd702667330ac3e23
Artem <mart_22@ukr.net>
2022-09-06 22:58:34 +0300

    Slider.js: Init reviews slider

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -1,1 +1,24 @@
-export default function sliders() {}
+export default function sliders() {
+	if (document.querySelector('.reviews__slider')) {
+		new Swiper('.reviews__slider', {
+			on: {
+				init() {
+					if (document.querySelector('.reviews__slider-wrapper')) {
+						document.querySelector('.reviews__slider-wrapper').classList.remove('style-3');
+					}
+				},
+			},
+			loop: true,
+			forceToAxis: true,
+			autoHeight: true,
+			navigation: {
+				nextEl: '.reviews__slider-wrapper .slider-btn--next',
+				prevEl: '.reviews__slider-wrapper .slider-btn--prev',
+			},
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+		});
+	}
+}
