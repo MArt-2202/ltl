d21fad4978d57aee8e9d6934163ad7aca247857c
Artem <mart_22@ukr.net>
2022-09-08 00:15:39 +0300

    Add search form autocomplete in blocksStyles.js

--- src/js/modules/blocksStyles.js
+++ src/js/modules/blocksStyles.js
@@ -6,4 +6,44 @@ export default function blockStyles() {
 			}
 		});
 	}
+
+	if (
+		document.querySelector('#search-form') &&
+		document.querySelector('#search-form__input') &&
+		document.querySelector('#search-form__list') &&
+		document.querySelector('#search-form__close')
+	) {
+		const input = document.querySelector('#search-form__input'),
+			list = document.querySelector('#search-form__list'),
+			closeBtn = document.querySelector('#search-form__close');
+
+		input.addEventListener('input', function (e) {
+			if (e.target.value.length) {
+				closeBtn.classList.remove('dn');
+			} else {
+				closeBtn.classList.add('dn');
+			}
+
+			if (e.target.value.length > 2) {
+				list.classList.add('show');
+			} else {
+				list.classList.remove('show');
+			}
+		});
+
+		closeBtn.addEventListener('click', function () {
+			input.value = '';
+			list.classList.remove('show');
+
+			this.classList.add('dn');
+		});
+
+		document.addEventListener('click', function (e) {
+			if (!e.target.closest('#search-form')) {
+				list.classList.remove('show');
+				input.value = '';
+				closeBtn.classList.add('dn');
+			}
+		});
+	}
 }
