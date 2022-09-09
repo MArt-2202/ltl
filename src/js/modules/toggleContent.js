9aeca6bf65fe1231f9a2e3b4ae8e52bf29c4264f
Artem <mart_22@ukr.net>
2022-09-09 12:43:03 +0300

    Edit toggleContent.js

--- src/js/modules/toggleContent.js
+++ src/js/modules/toggleContent.js
@@ -1,65 +1,21 @@
 export default function toggleContent() {
-	if (document.querySelector('header') && document.querySelector('.toggle-wrapper')) {
-		function toggleFuncAdd() {
-			document.querySelector('.overlay').addEventListener('click', function (e) {
-				document.querySelector('.toggle-btn').classList.remove('toggle-btn-style');
-				document.querySelector('.toggle-wrapper > div').classList.remove('content-visible');
-				e.target.classList.remove('overlay-visible');
+	document.addEventListener('click', toggleFunc());
 
-				document.body.style.overflowY = '';
-				document.body.style.paddingRight = '';
-			});
-		}
-		if ('ontouchstart' in document.documentElement) {
-			document
-				.querySelector('header')
-				.addEventListener('touchstart', toggleFunc(), { passive: true });
-			toggleFuncAdd();
-		} else {
-			document.querySelector('header').addEventListener('click', toggleFunc());
-			toggleFuncAdd();
-		}
+	function toggleFunc() {
+		return function (e) {
+			const target = e.target,
+				toggleWr = document.querySelector('.toggle-wrapper'),
+				toggleBtn = document.querySelector('.toggle-btn'),
+				toggleBtnEl = document.querySelectorAll('.toggle-btn span'),
+				toggleContent = document.querySelector('.toggle-wrapper > div'),
+				overlay = document.querySelector('.overlay');
 
-		function toggleFunc() {
-			return function (e) {
-				const target = e.target,
-					toggleWr = document.querySelector('.toggle-wrapper'),
-					toggleBtn = document.querySelector('.toggle-btn'),
-					toggleBtnEl = document.querySelectorAll('.toggle-btn span'),
-					toggleContent = document.querySelector('.toggle-wrapper > div'),
-					overlay = document.querySelector('.overlay');
-
-				for (let i = 0; i < toggleBtnEl.length; i++) {
-					if (target == toggleBtnEl[i]) {
-						toggleBtn.classList.toggle('toggle-btn-style');
-						toggleContent.classList.toggle('content-visible');
-						overlay.classList.toggle('overlay-visible');
-
-						if (document.querySelector('.toggle-btn-style')) {
-							document.body.style.paddingRight =
-								window.innerWidth - document.documentElement.clientWidth + 'px';
-							document.body.style.overflowY = 'hidden';
-						} else {
-							if (!document.querySelector('.modal-overlay.show')) {
-								document.body.style.overflowY = '';
-								document.body.style.paddingRight = '';
-
-								if (window.matchMedia('(max-width: 1200px)').matches) {
-									document.body.classList.toggle('body-toggle');
-								}
-
-								if (window.matchMedia('(min-width: 1201px)').matches) {
-									document.body.classList.remove('body-toggle');
-								}
-							}
-						}
-					}
-				}
-
-				if (target == toggleWr || target == toggleBtn) {
+			for (let i = 0; i < toggleBtnEl.length; i++) {
+				if (target == toggleBtnEl[i]) {
 					toggleBtn.classList.toggle('toggle-btn-style');
 					toggleContent.classList.toggle('content-visible');
 					overlay.classList.toggle('overlay-visible');
+
 					if (document.querySelector('.toggle-btn-style')) {
 						document.body.style.paddingRight =
 							window.innerWidth - document.documentElement.clientWidth + 'px';
@@ -71,21 +27,37 @@ export default function toggleContent() {
 						}
 					}
 				}
+			}
 
-				if (!target.closest('.toggle-wrapper')) {
-					toggleBtn.classList.remove('toggle-btn-style');
-					toggleContent.classList.remove('content-visible'),
-						overlay.classList.remove('overlay-visible');
-
-					if (
-						!document.querySelector('.modal-overlay.show') &&
-						window.matchMedia('(max-width: 1200px)').matches
-					) {
+			if (target == toggleWr || target == toggleBtn) {
+				toggleBtn.classList.toggle('toggle-btn-style');
+				toggleContent.classList.toggle('content-visible');
+				overlay.classList.toggle('overlay-visible');
+				if (document.querySelector('.toggle-btn-style')) {
+					document.body.style.paddingRight =
+						window.innerWidth - document.documentElement.clientWidth + 'px';
+					document.body.style.overflowY = 'hidden';
+				} else {
+					if (!document.querySelector('.modal-overlay.show')) {
 						document.body.style.overflowY = '';
 						document.body.style.paddingRight = '';
 					}
 				}
-			};
-		}
+			}
+
+			if (!target.closest('.toggle-wrapper')) {
+				toggleBtn.classList.remove('toggle-btn-style');
+				toggleContent.classList.remove('content-visible'),
+					overlay.classList.remove('overlay-visible');
+
+				if (
+					!document.querySelector('.modal-overlay.show') &&
+					window.matchMedia('(max-width: 1200px)').matches
+				) {
+					document.body.style.overflowY = '';
+					document.body.style.paddingRight = '';
+				}
+			}
+		};
 	}
 }
