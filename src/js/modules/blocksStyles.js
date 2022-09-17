f90384470d5785ad8748d887a38f9c4d1ca97156
Artem <mart_22@ukr.net>
2022-09-18 00:14:33 +0300

    Add intersecion observer for lazyload map in blocksStyles.js

--- src/js/modules/blocksStyles.js
+++ src/js/modules/blocksStyles.js
@@ -46,4 +46,26 @@ export default function blockStyles() {
 			}
 		});
 	}
+
+	if (document.querySelector('.lazyload-map')) {
+		const observer = new IntersectionObserver(
+			(entries, observer) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting && entry.target.dataset.src) {
+						entry.target.insertAdjacentHTML(
+							'beforeend',
+							`
+									<iframe src="${entry.target.dataset.src}" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+								`
+						);
+						observer.unobserve(entry.target);
+					}
+				});
+			},
+			{
+				rootMargin: '50px',
+			}
+		);
+		document.querySelectorAll('.lazyload-map').forEach((item) => observer.observe(item));
+	}
 }
