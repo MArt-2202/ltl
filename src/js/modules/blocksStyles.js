1d942eff0cc6b9510d92da1006e1a5b79a6d89f4
Artem <mart_22@ukr.net>
2022-09-28 16:52:31 +0300

    Edit blocksStyles.js

--- src/js/modules/blocksStyles.js
+++ src/js/modules/blocksStyles.js
@@ -1,41 +1,46 @@
 export default function blockStyles() {
+	if (document.querySelector('#toggle-wrapper-2')) {
+		document.body.classList.add('page-with-aside-fixed');
+	}
 	if (
-		document.querySelector('#search-form') &&
-		document.querySelector('#search-form__input') &&
-		document.querySelector('#search-form__list') &&
-		document.querySelector('#search-form__close')
+		document.querySelector('.autocomplete-form') &&
+		document.querySelector('.autocomplete-form__input') &&
+		document.querySelector('.autocomplete-form__list') &&
+		document.querySelector('.autocomplete-form__close')
 	) {
-		const input = document.querySelector('#search-form__input'),
-			list = document.querySelector('#search-form__list'),
-			closeBtn = document.querySelector('#search-form__close');
+		document.querySelectorAll('.autocomplete-form').forEach((el) => {
+			const input = el.querySelector('.autocomplete-form__input'),
+				list = el.querySelector('.autocomplete-form__list'),
+				closeBtn = el.querySelector('.autocomplete-form__close');
 
-		input.addEventListener('input', function (e) {
-			if (e.target.value.length) {
-				closeBtn.classList.remove('dn');
-			} else {
-				closeBtn.classList.add('dn');
-			}
+			input.addEventListener('input', function (e) {
+				if (e.target.value.length) {
+					closeBtn.classList.remove('dn');
+				} else {
+					closeBtn.classList.add('dn');
+				}
 
-			if (e.target.value.length > 2) {
-				list.classList.add('show');
-			} else {
-				list.classList.remove('show');
-			}
-		});
+				if (e.target.value.length > 2) {
+					list.classList.add('show');
+				} else {
+					list.classList.remove('show');
+				}
+			});
 
-		closeBtn.addEventListener('click', function () {
-			input.value = '';
-			list.classList.remove('show');
-
-			this.classList.add('dn');
-		});
-
-		document.addEventListener('click', function (e) {
-			if (!e.target.closest('#search-form')) {
-				list.classList.remove('show');
+			closeBtn.addEventListener('click', function () {
 				input.value = '';
-				closeBtn.classList.add('dn');
-			}
+				list.classList.remove('show');
+
+				this.classList.add('dn');
+			});
+
+			document.addEventListener('click', function (e) {
+				if (!e.target.closest('.autocomplete-form')) {
+					list.classList.remove('show');
+					input.value = '';
+					closeBtn.classList.add('dn');
+				}
+			});
 		});
 	}
 
